6c59d0f3fac2a8a6efd4e2c32ec4ea99b314b914
Jack Webb-Heller <627533+jackwh@users.noreply.github.com>
2025-02-17 15:16:53 +0000

    Update icon-column.blade.php
    Fix bug preventing Blade view caching

--- resources/views/tables/icon-column.blade.php
+++ resources/views/tables/icon-column.blade.php
@@ -1,5 +1,6 @@
 <div class="filament-icon-picker-icon-column px-4 py-3">
 	@if($icon = $getState())
-		<x-icon class="h-6" name="{{$icon}}" />
+		{{-- <x-icon class="h-6" name="{{$icon}}" /> --}}
+		<x-dynamic-component :component="$icon" @class(['h-6']) />
 	@endif
 </div>
